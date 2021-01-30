714988d086a17de62aaf24ffb963df116f94149b
WizArdZ3658 <p.somnath2599@gmail.com>
2021-01-30 23:14:47 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,65 @@
+var http = require('http')
+var url = require('url');
+var MongoClient = require('mongodb').MongoClient;
+var url1 = process.env.MONGODB_UNIVERSITY_PROJECT;
+var dbname = "university";
+
+const server = http.createServer((req, res) => {
+    const headers = {
+        'Content-Type' : 'application/json',
+        'Access-Control-Allow-Origin': '*',
+    };
+
+    var q = url.parse(req.url, true);
+    var qdata = q.query;
+
+    if (q.pathname === '/api/colleges') {
+        MongoClient.connect(url1, (err, db) => {
+            if (err) {
+                throw err;
+            }
+            var dbo = db.db(dbname);
+            dbo.collection("colleges").find({}).toArray((err, result) => {
+                if (err) {
+                    throw err;
+                }
+
+                res.writeHead(200, headers);
+                res.end(JSON.stringify(result));
+            });
+        });
+    }
+    else if (q.pathname === '/api/students') {
+
+        MongoClient.connect(url1, (err, db) => {
+            if (err) {
+                throw err;
+            }
+            var dbo = db.db(dbname);
+            var query;
+            
+            if (qdata.collegeid) {
+                query = { college_id: parseInt(qdata.collegeid) };
+            }
+            else {
+                query = {};
+            }
+
+
+            dbo.collection("students").find(query).toArray((err, result) => {
+                if (err) {
+                    throw err;
+                }
+
+                res.writeHead(200, headers);
+                res.end(JSON.stringify(result));
+            });
+        });
+    }
+
+
+});
+
+const PORT = 8080;
+
+server.listen(PORT, () => console.log("Server running"));
